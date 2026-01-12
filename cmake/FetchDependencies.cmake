a131ac228db82303b5fd9836a0c0f38e245c57f7
lucabolzonello <labolzonello@gmail.com>
2026-01-12 12:36:31 -0800

    build: Established Catch2 and CTest setup for Unit Tests.
    Using Catch2 because it is an adequate testing library that I already have familiarity with.

---
+++ cmake/FetchDependencies.cmake
@@ -0,0 +1,10 @@
+include(FetchContent)
+
+function(fetch_catch2)
+    FetchContent_Declare(
+            catch2
+            GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+            GIT_TAG v3.11.0
+    )
+    FetchContent_MakeAvailable(catch2)
+endfunction()
