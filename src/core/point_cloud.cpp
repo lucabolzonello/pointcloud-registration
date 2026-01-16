fb3025b9c39f0363035ecde9c4c9d9a3809ee24e
lucabolzonello <labolzonello@gmail.com>
2026-01-16 12:40:51 -0800

    impl: Created test file for the PointCloud class
    Vector based implementation for PointCloud implemented. Note that I am keeping the comments fairly light, this is because I'm hoping that the test cases serve as the primary documentation. Also this class so far is pretty self-explanatory.  May revise this approach later depending on how it goes.

---
+++ src/core/point_cloud.cpp
@@ -0,0 +1,33 @@
+#include "pcr/core/point_cloud.hpp"
+#include <vector>
+
+namespace pcr::core {
+// Default Constructor
+PointCloud::PointCloud() : m_points() {}
+
+// size(), returns number of points in point cloud
+std::size_t PointCloud::size() const noexcept { return m_points.size(); }
+
+// reserve(), reserve memory for the n points to be stored in point cloud.
+// Slightly breaks abstraction but is crucial for efficiency.
+void PointCloud::reserve(std::size_t n) { m_points.reserve(n); }
+
+// is_empty(), returns true if there are no points in the point cloud
+bool PointCloud::is_empty() const noexcept { return this->size() == 0; }
+
+// add(), add a point to the point cloud
+void PointCloud::add(point_type p) { m_points.push_back(p); }
+
+// begin(), returns iterator to beginning element
+PointCloud::point_iterator PointCloud::begin() { return m_points.begin(); }
+PointCloud::const_point_iterator PointCloud::cbegin() const {
+  return m_points.cbegin();
+}
+
+// end(), returns the past-the-end iterator
+PointCloud::point_iterator PointCloud::end() { return m_points.end(); }
+PointCloud::const_point_iterator PointCloud::cend() const {
+  return m_points.cend();
+}
+
+} // namespace pcr::core
