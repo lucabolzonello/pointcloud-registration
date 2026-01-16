56226486a18fa82c7781d1c84f78f01c0682d7fb
lucabolzonello <labolzonello@gmail.com>
2026-01-16 12:32:13 -0800

    impl: Created header file for the PointCloud class
    For now this PointCloud class is essentially a just a vector with a different name. But in the future it will have other uses.
    
    I will likely also want to create an emplace_back() feature in the future to avoid unnecessary construction and copying.

---
+++ include/pcr/core/point_cloud.hpp
@@ -0,0 +1,63 @@
+#ifndef POINT_CLOUD_HPP
+#define POINT_CLOUD_HPP
+
+#include "point.hpp"
+#include <cstddef>
+#include <vector>
+
+namespace pcr::core {
+
+class PointCloud {
+public:
+  // The data type of every coordinate in the PointCloud
+  using coordinate_value_type = float;
+
+  using point_type = Point<coordinate_value_type>;
+
+  using point_iterator = std::vector<point_type>::iterator;
+
+  using const_point_iterator = std::vector<point_type>::const_iterator;
+
+  // Default Constructor
+  PointCloud();
+
+  // PointCloud(Iterator), Construct from given iterator, leaves source intact
+  template <typename Iterator>
+  PointCloud(Iterator const first, Iterator const last)
+      : m_points(first, last) {}
+
+  // size(), returns number of points in point cloud
+  [[nodiscard]] std::size_t size() const noexcept;
+
+  // reserve(), reserve memory for the n points to be stored in point cloud.
+  // Slightly breaks abstraction but is crucial for efficiency.
+  void reserve(std::size_t n);
+
+  // is_empty(), returns true if there are no points in the point cloud
+  [[nodiscard]] bool is_empty() const noexcept;
+
+  // add(), add a point to the point cloud
+  void add(point_type p);
+
+  // add(Iterator), add points from given iterator to the point cloud, leaves
+  // source intact
+  template <typename Iterator>
+  void add(Iterator const first, Iterator const last) {
+    m_points.insert(m_points.end(), first, last);
+  }
+
+  // begin(), returns iterator to beginning element
+  [[nodiscard]] point_iterator begin();
+  [[nodiscard]] const_point_iterator cbegin() const;
+
+  // end(), returns the past-the-end iterator
+  [[nodiscard]] point_iterator end();
+  [[nodiscard]] const_point_iterator cend() const;
+
+private:
+  std::vector<point_type> m_points;
+};
+
+} // namespace pcr::core
+
+#endif
