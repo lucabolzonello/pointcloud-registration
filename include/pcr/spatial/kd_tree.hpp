fe9fbdddee9588c95dd636701fca5d542fe73258
lucabolzonello <labolzonello@gmail.com>
2026-02-02 17:01:10 -0800

    impl: Implemented KdTree Construction
    Decided to implement it as a contiguous vector with an implicit tree ordering.
    
    The logic is as follows:
    The left child is at 2*idx + 1
    The right child is at 2*idx + 2
    The parent is at floor((idx - 1) / 2)
    
    This was done to avoid the overhead of having to hold 3 pointers (left, right, parent) at each node since this would drastically increase the size of our KdTreeNode which isn't ideal with point cloud data.
    
    One downside to this is that I have to construct the tree in a balanced fashion so that the vectors size is deterministic and bounded. This results in me having to compute the exact median value for each plane rather than being able to estimate a split point.

--- include/pcr/spatial/kd_tree.hpp
+++ include/pcr/spatial/kd_tree.hpp
@@ -10,6 +10,11 @@
 
 namespace pcr::spatial {
 
+struct KdTreeNode {
+  float split_value;   // 4 bytes
+  uint8_t split_plane; // 1 byte
+}; // 8 byte aligned
+
 class KdTree {
 public:
   using point_type = core::PointCloud::point_type;
@@ -24,9 +29,11 @@ public:
   KdTree(const std::vector<coordinate_value_type point_type::*> &dimensions);
 
   // build_index(cloud)
+  // The provided cloud object will have its points rearranged but not changed
+  // in value
   // Note: It is assumed that the cloud must outlive this KdTree
   // object, undefined behaviour if caller doesn't follow this guideline
-  void build_index(const core::PointCloud &cloud);
+  void build_index(core::PointCloud &cloud);
 
   // knn_search(query_point, k, &out_indices, &out_distances_squared), Get the K
   // nearest neighbours to the query_point
@@ -45,9 +52,23 @@ public:
       std::vector<coordinate_value_type> &out_distances_squared) const;
 
 private:
+  // Small vector containing point-to-member to the values in the point_type
+  // class we want to treat as dimensions
+  const std::vector<coordinate_value_type point_type::*> m_dimensions = {
+      &point_type::x, &point_type::y, &point_type::z};
 
   // Tree stored as a vector, must ensure I build tree in fully balanced fashion
   std::vector<KdTreeNode> tree;
+
+  // build_index_rec(), recursive function for building the KdTree
+  // params:
+  //  cloud: Reference to PointCloud
+  //  left:  Index of the leftmost element in the current window
+  //  right: Index 1-past the rightmost element in the current window
+  //  index: Index of the tree that this node should be written to
+  //  split_plane: Represents the current dimension to split along for the tree
+  void build_index_rec(pcr::core::PointCloud &cloud, long left, long right,
+                       long tree_idx, uint8_t split_plane);
 };
 
 } // namespace pcr::spatial
--- src/spatial/kd_tree.cpp
+++ src/spatial/kd_tree.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 
 #include "pcr/spatial/kd_tree.hpp"
 
@@ -11,6 +12,52 @@ pcr::spatial::KdTree::KdTree(
     throw std::runtime_error(
         "The KDTree must be indexed over  0 < K <= 255 dimensions");
   }
+}
+
+void KdTree::build_index(pcr::core::PointCloud &cloud) {
+  // reserve capacity in tree ahead of time to prevent repeated resizes
+  tree.reserve(cloud.size());
+
+  // Recursively build index
+  build_index_rec(cloud, 0, cloud.size(), 0, 0);
+}
+
+void KdTree::build_index_rec(pcr::core::PointCloud &cloud, long left,
+                             long right, long tree_idx, uint8_t split_plane) {
+
+  long num_elements = right - left;
+
+  // Stop recursion if leaf node
+  if (num_elements <= 0) {
+    return;
+  }
+  if (num_elements == 1) {
+    tree[tree_idx] = {cloud[left].*(m_dimensions[split_plane]), split_plane};
+    return;
+  }
+
+  // determine midpoint between right and left
+  long midpoint = left + num_elements / 2;
+
+  // call nth_element to find the median and place it at midpoint_it
+  std::nth_element(cloud.begin() + left, cloud.begin() + midpoint,
+                   cloud.begin() + right,
+                   [&split_plane, this](const auto &lhs, const auto &rhs) {
+                     return lhs.*(m_dimensions[split_plane]) <
+                            rhs.*(m_dimensions[split_plane]);
+                   });
+
+  // Add midpoint to tree, this is the split node
+  tree[tree_idx] = {cloud[midpoint].*(m_dimensions[split_plane]), split_plane};
+
+  // increment split_plane
+  split_plane = (split_plane + 1) % m_dimensions.size();
+
+  // Recurse left side
+  build_index_rec(cloud, left, midpoint, 2 * tree_idx + 1, split_plane);
+
+  // Recurse right side
+  build_index_rec(cloud, midpoint + 1, right, 2 * tree_idx + 2, split_plane);
 }
 
 } // namespace pcr::spatial
