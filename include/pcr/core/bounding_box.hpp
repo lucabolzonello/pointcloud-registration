5d8fd5bbc68305bd1396571fa94565d5b227558a
lucabolzonello <labolzonello@gmail.com>
2026-02-04 10:14:56 -0800

    docs: Updated inline documentation for BoundingBox to use Doxygen

--- include/pcr/core/bounding_box.hpp
+++ include/pcr/core/bounding_box.hpp
@@ -1,9 +1,34 @@
+/**
+ * @file bounding_box.hpp
+ * @brief Axis-aligned bounding box for spatial queries
+ */
+
 #ifndef BOUNDING_BOX_HPP
 #define BOUNDING_BOX_HPP
+
 #include <limits>
+
+/**
+ * @namespace pcr::core
+ * @brief Core data structures for point cloud registration
+ */
 namespace pcr::core {
-// Struct to represent a 3-Dimensional bounding box
+
+/**
+ * @brief 3D Spatial Bounding Box
+ *
+ * Represents 3D bounding of a point cloud. Initialized with inverted bounds to
+ * simplify incremental updates.
+ *
+ * @tparam T Type for coordinates (float or double)
+ */
 template <typename T> struct BoundingBox {
+  /**
+   * @brief Default constructor - initializes inverted bounds
+   *
+   * Min values are set to numeric max, max values to numeric lowest when
+   * initialized.
+   */
   BoundingBox()
       : min_x(std::numeric_limits<T>::max()),
         min_y(std::numeric_limits<T>::max()),
@@ -12,13 +37,14 @@ template <typename T> struct BoundingBox {
         max_y(std::numeric_limits<T>::lowest()),
         max_z(std::numeric_limits<T>::lowest()) {}
 
-  T min_x;
-  T min_y;
-  T min_z;
-  T max_x;
-  T max_y;
-  T max_z;
+  T min_x; ///< Minimum X-coordinate of bounding box
+  T min_y; ///< Minimum Y-coordinate of bounding box
+  T min_z; ///< Minimum Z-coordinate of bounding box
+  T max_x; ///< Maximum X-coordinate of bounding box
+  T max_y; ///< Maximum Y-coordinate of bounding box
+  T max_z; ///< Maximum Z-coordinate of bounding box
 };
 
 } // namespace pcr::core
-#endif
+
+#endif // BOUNDING_BOX_HPP
