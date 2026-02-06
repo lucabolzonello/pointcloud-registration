4230dab6a5b79053a424109b28f39ee1e0297e3b
lucabolzonello <labolzonello@gmail.com>
2026-02-06 10:33:45 -0800

    impl: Converted Point class to use the new project wide types defined in prelude.hpp

--- tests/core/test_point.cpp
+++ tests/core/test_point.cpp
@@ -3,84 +3,81 @@
 
 #include "pcr/core/point.hpp"
 
-using test_types = std::tuple<std::size_t, float, double>;
+#include "pcr/prelude.hpp"
 
-TEMPLATE_LIST_TEST_CASE(
-    "Point is default constructable, and uses zero initialization",
-    "[constructor]", test_types) {
-  pcr::core::Point<TestType> p1;
-  CHECK(p1.x == TestType{});
-  CHECK(p1.y == TestType{});
-  CHECK(p1.z == TestType{});
+TEST_CASE("Point is default constructable, and uses zero initialization",
+          "[constructor]") {
+  pcr::core::Point<pcr::coord_t> p1;
+  CHECK(p1.x == pcr::coord_t{});
+  CHECK(p1.y == pcr::coord_t{});
+  CHECK(p1.z == pcr::coord_t{});
 }
 
-TEMPLATE_LIST_TEST_CASE("Constructor with arguments initializes coordinates",
-                        "[constructor]", test_types) {
+TEST_CASE("Constructor with arguments initializes coordinates",
+          "[constructor]") {
 
-  auto x_value = static_cast<TestType>(1);
-  auto y_value = static_cast<TestType>(2);
-  auto z_value = static_cast<TestType>(3);
+  auto x_value = static_cast<pcr::coord_t>(1);
+  auto y_value = static_cast<pcr::coord_t>(2);
+  auto z_value = static_cast<pcr::coord_t>(3);
 
-  pcr::core::Point<TestType> p1(x_value, y_value, z_value);
+  pcr::core::Point<pcr::coord_t> p1(x_value, y_value, z_value);
   CHECK(p1.x == x_value);
   CHECK(p1.y == y_value);
   CHECK(p1.z == z_value);
 }
 
-TEMPLATE_LIST_TEST_CASE("Point coordinates can be modified", "[member access]",
-                        test_types) {
-  auto x_value = static_cast<TestType>(1);
-  auto y_value = static_cast<TestType>(2);
-  auto z_value = static_cast<TestType>(3);
+TEST_CASE("Point coordinates can be modified", "[member access]") {
+  auto x_value = static_cast<pcr::coord_t>(1);
+  auto y_value = static_cast<pcr::coord_t>(2);
+  auto z_value = static_cast<pcr::coord_t>(3);
 
-  pcr::core::Point<TestType> p1(x_value, y_value, z_value);
+  pcr::core::Point<pcr::coord_t> p1(x_value, y_value, z_value);
 
   SECTION("Set X Coordinate") {
-    auto new_x_value = static_cast<TestType>(99);
+    auto new_x_value = static_cast<pcr::coord_t>(99);
     p1.x = new_x_value;
     CHECK(p1.x == new_x_value);
   }
 
   SECTION("Set Y Coordinate") {
-    auto new_y_value = static_cast<TestType>(10);
+    auto new_y_value = static_cast<pcr::coord_t>(10);
     p1.y = new_y_value;
     CHECK(p1.y == new_y_value);
   }
 
   SECTION("Set Z Coordinate") {
-    auto new_z_value = static_cast<TestType>(42);
+    auto new_z_value = static_cast<pcr::coord_t>(42);
     p1.z = new_z_value;
     CHECK(p1.z == new_z_value);
   }
 }
 
-TEMPLATE_LIST_TEST_CASE("Operator== overload works", "[equivalency overload]",
-                        test_types) {
-  auto x_value = static_cast<TestType>(1);
-  auto y_value = static_cast<TestType>(2);
-  auto z_value = static_cast<TestType>(3);
+TEST_CASE("Operator== overload works", "[equivalency overload]") {
+  auto x_value = static_cast<pcr::coord_t>(1);
+  auto y_value = static_cast<pcr::coord_t>(2);
+  auto z_value = static_cast<pcr::coord_t>(3);
 
   SECTION("Points equivalent") {
 
-    pcr::core::Point<TestType> p1(x_value, y_value, z_value);
-    pcr::core::Point<TestType> p2(x_value, y_value, z_value);
+    pcr::core::Point<pcr::coord_t> p1(x_value, y_value, z_value);
+    pcr::core::Point<pcr::coord_t> p2(x_value, y_value, z_value);
   }
 
   SECTION("Different X value") {
 
-    pcr::core::Point<TestType> p1(x_value, y_value, z_value);
-    pcr::core::Point<TestType> p2(y_value, y_value, z_value);
+    pcr::core::Point<pcr::coord_t> p1(x_value, y_value, z_value);
+    pcr::core::Point<pcr::coord_t> p2(y_value, y_value, z_value);
   }
 
   SECTION("Different Y value") {
 
-    pcr::core::Point<TestType> p1(x_value, y_value, z_value);
-    pcr::core::Point<TestType> p2(x_value, z_value, z_value);
+    pcr::core::Point<pcr::coord_t> p1(x_value, y_value, z_value);
+    pcr::core::Point<pcr::coord_t> p2(x_value, z_value, z_value);
   }
 
   SECTION("Different Z value") {
 
-    pcr::core::Point<TestType> p1(x_value, y_value, z_value);
-    pcr::core::Point<TestType> p2(x_value, y_value, x_value);
+    pcr::core::Point<pcr::coord_t> p1(x_value, y_value, z_value);
+    pcr::core::Point<pcr::coord_t> p2(x_value, y_value, x_value);
   }
 }
