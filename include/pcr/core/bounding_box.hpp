d73970f8124f0414bcb6202a36ca989ffa24117d
lucabolzonello <labolzonello@gmail.com>
2026-03-11 14:26:38 -0700

    impl: Added BoundingBox split method to split the bounding box along a dimension. Also officially supports the Copy Constructor now.

--- include/pcr/core/bounding_box.hpp
+++ include/pcr/core/bounding_box.hpp
@@ -37,6 +37,50 @@ template <typename T> struct BoundingBox {
         max_y(std::numeric_limits<T>::lowest()),
         max_z(std::numeric_limits<T>::lowest()) {}
 
+  /**
+   * @brief Copy Constructor
+   *
+   * Copies the bounding box.
+   *
+   * @param other The bounding box to copy
+   */
+  BoundingBox(const BoundingBox &other) = default;
+
+  /**
+   * @brief Split the bounding box along a dimension
+   *
+   * Modifies the bounding box to contain either the left or right side of the
+   * split. If left_side is true, the bounding box will contain the left side of
+   * the split. Otherwise, the bounding box will contain the right side of the
+   * split.
+   *
+   * @param left_side Whether to take the left side of the split or the right
+   * side
+   * @param split_dimension The dimension to split along (0 for x, 1 for y, 2
+   * for z)
+   */
+  void split(bool left_side, uint8_t split_dimension) {
+    if (split_dimension == 0) {
+      if (left_side) {
+        max_x = (min_x + max_x) / 2;
+      } else {
+        min_x = (min_x + max_x) / 2;
+      }
+    } else if (split_dimension == 1) {
+      if (left_side) {
+        max_y = (min_y + max_y) / 2;
+      } else {
+        min_y = (min_y + max_y) / 2;
+      }
+    } else if (split_dimension == 2) {
+      if (left_side) {
+        max_z = (min_z + max_z) / 2;
+      } else {
+        min_z = (min_z + max_z) / 2;
+      }
+    }
+  }
+
   T min_x; ///< Minimum X-coordinate of bounding box
   T min_y; ///< Minimum Y-coordinate of bounding box
   T min_z; ///< Minimum Z-coordinate of bounding box
