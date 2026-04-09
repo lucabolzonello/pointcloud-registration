c73c55fbcb999ebc9dbce7b1cf6f48983f317af9
lucabolzonello <labolzonello@gmail.com>
2026-04-08 21:39:40 -0700

    build: Added fetch_eigen so that I can include eigen in my library
    Eigen will be critical for the SVD and Rigid Body Transformation portion of the project.

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -17,5 +17,13 @@ function(fetch_google_benchmark)
     )
     set(BENCHMARK_ENABLE_GTEST_TESTS OFF CACHE BOOL "" FORCE) # to get a
     FetchContent_MakeAvailable(benchmark)
+endfunction()
 
+function(fetch_eigen)
+    FetchContent_Declare(
+            eigen
+            GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+            GIT_TAG 3.4.0
+    )
+    FetchContent_MakeAvailable(eigen)
 endfunction()
