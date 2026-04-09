1d90b211299321f302cb104df0015de67b99782d
lucabolzonello <labolzonello@gmail.com>
2026-04-08 21:44:05 -0700

    build: Added test_icp.cpp, icp.hpp placeholder and corresponding CMakeLists.txt file
    I'm honestly not sure how I'm planning to unit test ICP? Feels really tough for me to come up with comprehensive tests. Will have to think more about this and come up with an appropriate methodology.

---
+++ include/pcr/registration/icp.hpp
@@ -0,0 +1,9 @@
+#ifndef ICP_HPP
+#define ICP_HPP
+
+#include <Eigen/Dense>
+#include "pcr/prelude.hpp"
+#include "pcr/core/point_cloud.hpp"
+#include "pcr/spatial/kd_tree.hpp"
+
+#endif
---
+++ tests/registration/test_icp.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include <algorithm>
+//#include <catch2/catch_all.hpp>
+#include <iostream>
+#include <vector>
+
+#include "pcr/core/point_cloud.hpp"
+#include "pcr/spatial/kd_tree.hpp"
+#include "pcr/registration/icp.hpp"
+
+
